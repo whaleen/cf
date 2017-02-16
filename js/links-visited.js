1640edc38f83c46c96f31af2875fb4db80e33369
Joshua Vaage <joshuavaage@gmail.com>
2017-02-15 19:16:47 -0800

    Links visited local storage

---
+++ js/links-visited.js
@@ -0,0 +1,8 @@
+localStorage.setItem('visited-'+window.location.pathname,true);
+var links = document.getElementsByTagName('a');
+for (i=0;i<links.length;i++) {
+  var link = links[i];
+  if (link.host == window.location.host && localStorage.getItem('visited-' + link.pathname + '/')) {
+    link.dataset.visited = true;
+  }
+}
